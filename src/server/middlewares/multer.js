26eb505af53e664ab50aebe30877e394303adfad
akash450 <75708031+akash450@users.noreply.github.com>
2023-07-01 14:04:22 +1000

    Delete src/server directory

--- src/server/middlewares/multer.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-//temp storage
-const storage = multer.memoryStorage();
-
-export const singleUpload = multer({
-    storage
-}).single("file");
--- src/server/utils/error.js
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-        //send to error class
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
-
-export default ErrorHandler;
